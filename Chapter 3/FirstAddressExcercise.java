44aae88ff2b39ca3c3ed17acbb2cb3d73990a01b
KhylleVillasurda <146078285+KhylleVillasurda@users.noreply.github.com>
2024-09-20 08:19:41 -0700

    Add files via upload

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,11 @@
+public class  FirstAddressExcercise
+{
+	public static void main(String[] args) {
+		System.out.println("ROG(ASUS)");
+		displayAddress();
+	}
+	
+	public static void displayAddress() {
+	    System.out.println("Taipei City 112, Taiwan\nNo. 115 Lide Rd, Beitou District\nASUSTEK COMPUTER INCORPORATION");
+	}
+}
