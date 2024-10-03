8e0ec9310bee4224c6c31a3b8a232e51da72b537
KhylleVillasurda <146078285+KhylleVillasurda@users.noreply.github.com>
2024-10-03 11:12:43 -0700

    Add files via upload

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,32 @@
+class MathTest {
+    public static void main(String[] args) {
+     //yields the result of the square root of the number
+     System.out.println("SQRT:  "+ Math.sqrt(10));
+     
+     // return the sine, cosine
+     System.out.println("SIN:  "+ Math.sin(10));
+     System.out.println("COS:  "+ Math.cos(10));
+     System.out.println("FLOOR:  "+ Math.floor(10.5));
+     
+     //rounding up resulting in int
+     System.out.println("CEIL:  "+ Math.ceil(10.5));
+     
+     //rounding up resulting in doubles
+     System.out.println("ROUND:  "+ Math.round(10.5));
+     
+     //gives out the highest number
+     System.out.println("MAX:  "+ Math.max(10,20));
+     
+     //gives out the lowest number
+     System.out.println("MIN:  "+ Math.min(10,20));
+     
+     
+     //Generates Random Numbers from 0.0 to 0.1
+     System.out.println("RANDOM:  "+ Math.random());
+     
+     /*
+     So in conclusion some code that needs formulas can be achieved with these shortcuts!
+     */
+     
+    }
+}
